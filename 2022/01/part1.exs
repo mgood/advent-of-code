adc25518e2fa8f0bf8093f84fe9935c4cc3b9907
Matt Good <29016+mgood@users.noreply.github.com>
2022-12-01 12:05:43 -0800

    D01P1: chunk_while -> reduce

--- 2022/01/part1.exs
+++ 2022/01/part1.exs
@@ -8,25 +8,24 @@ defmodule Day1 do
     end
   end
 
-  def chunked(lines) do
-    Enum.chunk_while(
-      lines,
-      {0, 0},
-      fn elem, {max, curr} ->
-        case Integer.parse(elem) do
-          {x, _} -> {:cont, {max, curr + x}}
-          :error -> {:cont, {top(max, curr), 0}}
+  def reduced(lines) do
+    {max, _} =
+      Enum.reduce(
+        lines,
+        {0, 0},
+        fn elem, {max, curr} ->
+          case Integer.parse(elem) do
+            {x, _} -> {max, curr + x}
+            :error -> {top(max, curr), 0}
+          end
         end
-      end,
-      fn
-        {max, curr} -> {:cont, top(max, curr), {0, 0}}
-      end
-    )
+      )
+
+    max
   end
 end
 
 IO.read(:stdio, :all)
 |> String.split("\n")
-|> Day1.chunked()
-|> List.first()
+|> Day1.reduced()
 |> IO.puts()
