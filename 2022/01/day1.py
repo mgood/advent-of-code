87ef9c12e34c211027807f2055daf194faa66d0b
Matt Good <29016+mgood@users.noreply.github.com>
2022-12-01 10:02:10 -0800

    2022 Day 1

---
+++ 2022/01/day1.py
@@ -0,0 +1,15 @@
+import heapq
+
+def scores(lines):
+	curr = 0
+	for line in fp:
+		line = line.strip()
+		if line:
+			curr += int(line)
+		else:
+			yield curr
+			curr = 0
+
+with open('input.txt') as fp:
+	best = heapq.nlargest(3, scores(fp))
+print(sum(best))
