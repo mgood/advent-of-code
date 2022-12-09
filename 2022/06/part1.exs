790b4e86a7e9fd52450da99cc2043bf64e97676d
Matt Good <29016+mgood@users.noreply.github.com>
2022-12-08 20:23:42 -0800

    Day 6

---
+++ 2022/06/part1.exs
@@ -0,0 +1,16 @@
+#!/usr/bin/env elixir
+defmodule Day6 do
+  def answer(input) do
+    n = 4
+
+    n +
+      (input
+       |> String.to_charlist()
+       |> Enum.chunk_every(n, 1)
+       |> Enum.find_index(&(MapSet.size(MapSet.new(&1)) == n)))
+  end
+end
+
+IO.read(:stdio, :all)
+|> Day6.answer()
+|> IO.inspect()
