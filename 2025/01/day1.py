c83b3304e2cb2b95e29249bbdfc585315a57e64b
Matt Good <29016+mgood@users.noreply.github.com>
2025-12-02 10:45:55 -0800

    Day 1 part 2

--- 2025/01/day1.py
+++ 2025/01/day1.py
@@ -6,13 +6,29 @@ def rotate(start: int, rotation: str):
     return (start + parse(rotation)) % 100
 
 
+def count_clicks(start: int, rotation: str):
+    # there's a smarter solution using something like end // 100 to get the
+    # count, but that was getting tricky with checking for cases like starting
+    # at 0, so brute-force seemed like the simpler approach
+    rot = parse(rotation)
+    inc = 1 if rot > 0 else -1
+    pos = start
+    clicks = 0
+    while rot != 0:
+        pos += inc
+        pos %= 100
+        if pos == 0:
+            clicks += 1
+        rot -= inc
+    return pos, clicks
+
+
 if __name__ == "__main__":
     import sys
 
     pos = 50
     count = 0
     for line in sys.stdin:
-        pos = rotate(pos, line)
-        if pos == 0:
-            count += 1
+        pos, clicks = count_clicks(pos, line)
+        count += clicks
     print(count)
--- 2025/01/day1_test.py
+++ 2025/01/day1_test.py
@@ -1,5 +1,5 @@
 import pytest
-from day1 import parse, rotate
+from day1 import count_clicks, parse, rotate
 
 
 @pytest.mark.parametrize(
@@ -27,3 +27,18 @@ def test_parse(rotation: str, expected: int):
 )
 def test_rotate(start: int, rotation: str, expected: int):
     assert rotate(start, rotation) == expected
+
+
+@pytest.mark.parametrize(
+    "start,rotation,expected",
+    [
+        (50, "L68", (82, 1)),
+        (82, "L30", (52, 0)),
+        (52, "R48", (0, 1)),
+        (0, "L5", (95, 0)),
+        (95, "R60", (55, 1)),
+        (50, "R1000", (50, 10)),
+    ],
+)
+def test_clicks(start: int, rotation: str, expected: tuple[int, int]):
+    assert count_clicks(start, rotation) == expected
