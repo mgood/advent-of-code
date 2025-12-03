da92821b0717d3b2d28bbeabe6f4b87f3ac37ad0
Matt Good <29016+mgood@users.noreply.github.com>
2025-12-03 11:00:59 -0800

    Day 3 part 2

--- 2025/03/day3_test.py
+++ 2025/03/day3_test.py
@@ -13,3 +13,16 @@ from day3 import max_joltage
 )
 def test_max_joltage(batteries, expected):
     assert max_joltage(batteries) == expected
+
+
+@pytest.mark.parametrize(
+    "batteries,expected",
+    [
+        (987654321111111, 987654321111),
+        (811111111111119, 811111111119),
+        (234234234234278, 434234234278),
+        (818181911112111, 888911112111),
+    ],
+)
+def test_max_joltage2(batteries, expected):
+    assert max_joltage(batteries, num_enabled=12) == expected
